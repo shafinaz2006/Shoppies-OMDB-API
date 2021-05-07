3d93dc3edc2ff71bdda1cc418f9fd23f0c934986
Shafinaz Shafique <shafinaz2006@gmail.com>
2021-05-06 22:45:21 -0700

    add hook and styles, localStorage

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,66 +1,87 @@
 import React from 'react';
-import {useState} from 'react';
-import axios from 'axios';
 import './Home.scss';
+import star from '../../Assets/icons/star.svg';
+import useSearchForm from '../../useSearchForm';
 
-const Home = () =>{
-    const [searchTitle, setSearchTitle] = useState(''); 
-    const [movieList, setMovieList] = useState([]);
-    const handleSearch = (event) =>{
-        setSearchTitle(event.target.value);
-    }
-    const handleKeyPress = (event) =>{
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            console.log('do validate');
-            getMovieList(searchTitle);
-        }
-    }
-    const getMovieList = (search) =>{
-        console.log(search)
-        axios
-            .get(`http://www.omdbapi.com/?apikey=4bb50978&s=${search}`)
-            .then(response =>{
-                console.log(response.data.Search);
-                setMovieList([...response.data.Search])
-            })
-            .catch(error => console.log('error in movie data', error));
-    }
-    console.log(movieList);
-    return(
-        
+const Home = () => {
+    const {searchTitle, movieList, movieNotFound, nominations, 
+           handleSearch, handleKeyPress,
+           handleAddNominate, handleRemoveNominate} = useSearchForm();
+    
+    return (
         <section className='home'>
             <h1 className='home__heading'>The Shoppies</h1>
             <form className='home__movieSearch'>
                 <label className='input-label' htmlFor='movieTitle'>Movie title </label>
-                <input className='input input--search' name='movieTitle'  placeholder='movie title'
+                <input className='input input--search' name='movieTitle' placeholder='movie title'
                     value={searchTitle} onKeyPress={handleKeyPress}
                     onChange={handleSearch}
                 />
             </form>
             <div className='home__movieInfo'>
-                {movieList.length > 0? 
-                <div className='home__movieResult'>
-                    <h2 className='home__subHeading home__subHeading--movieResult'>Results for "{searchTitle}"</h2>
-                    <ul className='home__list'>
-                    {movieList.map(movie =>
-                        <li className='home__item' key={movie.imdbID}>
-                            {movie.Title} ({movie.Year})
-                            <button className='button button--nominate'>Nominate</button>
-                        </li>
-                    )
+                <div className= 'home__movieResult'>
+                    {movieNotFound || movieList <=0?
+                    <div className='home__messageHeading'>
+                        <p>
+                            {movieNotFound? `No movie found for "${searchTitle}"`: `Search any movie!!`}
+                        </p>
+                    </div>
+                    : ''
                     }
-                    </ul>
+                    {movieList.length > 0 ?
+                    <div>
+                        <h2 className='home__subHeading home__subHeading--movieResult'>Results for "{searchTitle}"</h2>
+                        <ul className='home__list'>
+                            {movieList.map(movie =>
+                                <li className='home__item' key={movie.imdbID}>
+                                    <span className='home__movieName'>
+                                        <img className='home__starIcon' src={star} alt='star icon'/>
+                                        {movie.Title} ({movie.Year})
+                                    </span>
+                                    <button className='button button--nominate' 
+                                            disabled={movie.nominated}
+                                            onClick={(event) => handleAddNominate(event, movie)}>
+                                            Nominate
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
+                    </div>: ''
+                    }
                 </div>
-                : 
+                
                 <div className='home__movieResult'>
-                    <h2 className='home__subHeading'>Search for a movie</h2>
-                </div>
-                }
-                <div className='home__nominations'>
-                    <h2 className='home__subHeading home__subHeading--movieResult'>
-                        Nominations
-                    </h2>
+                    {nominations.length > 0?
+                    <div>
+                        <h2 className='home__subHeading home__subHeading--movieResult'>
+                            Nominations
+                        </h2>
+                        {nominations.length >= 5? 
+                        <div className='home__messageHeading'>
+                            <p>You have nominated five movies!!!</p>
+                        </div>
+                        : ''
+                        }
+                        <ul className='home__list'>
+                            {nominations.map(nomination =>
+                                <li className='home__item' key={nomination.imdbID}>
+                                    <span className='home__movieName'>
+                                        <img className='home__starIcon' src={star} alt='star icon'/>
+                                        {nomination.Title} ({nomination.Year})
+                                    </span>
+                                    <button className='button button--nominate'
+                                            onClick={(event) => handleRemoveNominate(event, nomination)}>
+                                        Remove
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    : 
+                    <div className='home__messageHeading'>
+                        <p> Nominate your favourite movie!! </p>
+                    </div>
+                    }
                 </div>
             </div>
         </section>
---
+++ client/src/useSearchForm.js
@@ -0,0 +1,91 @@
+
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+const OMDB_API = process.env.REACT_APP_OMDB_API;
+
+const useSearchForm = () =>{
+    const [searchTitle, setSearchTitle] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [movieNotFound, setMovieNotFound] = useState(false)
+    const [nominations, setNominations] = useState([]);
+
+// useEffect to get nominations from localStorage:
+
+    useEffect(() =>{
+        let nominationslist = JSON.parse(window.localStorage.getItem('nominations'));
+        if(nominationslist) setNominations([...nominationslist])
+    }, []);
+
+// useEffect to set nominations in localStorage:
+
+    useEffect(() =>{
+        window.localStorage.setItem('nominations', JSON.stringify(nominations));
+    }, [nominations])
+    
+// search input onChange event handler:
+
+    const handleSearch = (event) => {
+        setSearchTitle(event.target.value);
+    }
+
+// get movie list when enter is pressed:
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            getMovieList(searchTitle);
+        }
+    }
+
+// add nominate handler:
+
+    const handleAddNominate = (event, nominatedMovie) =>{
+        // console.log(movie);
+        setNominations(prevArray => [...prevArray, nominatedMovie])
+        
+        let movieListUpdated = movieList.map(movie => 
+            movie.imdbID === nominatedMovie.imdbID?{...movie, nominated: true}: {...movie} 
+        );
+        setMovieList(movieListUpdated)
+    }
+
+// remove nominate handler:
+
+    const handleRemoveNominate = (event, nominatedMovie) =>{
+        let newNominateList = nominations.filter(nomination => nomination.imdbID !== nominatedMovie.imdbID );
+        setNominations([...newNominateList]);
+        // window.localStorage.setItem('nominations', JSON.stringify(nominations));
+        let movieListUpdated = movieList.map(movie => 
+            movie.imdbID === nominatedMovie.imdbID?{...movie, nominated: false}: {...movie} 
+        );
+        setMovieList(movieListUpdated)
+    }
+
+// axios request to get movieList:
+
+    const getMovieList = (search) => {
+        // console.log(search)
+        axios
+            .get(`http://www.omdbapi.com/?apikey=${OMDB_API}&s=${search}`)
+            .then(response => {
+                if(response.data.Search){
+                    let ids = response.data.Search.map(movie => movie.imdbID);
+
+                    // removes the duplicate data by imdbID:
+
+                    let list = response.data.Search.filter(
+                                    ({imdbID}, index) => !ids.includes(imdbID, index+1));
+                    let movieList = list.map(movie => ({...movie, nominated: false}));
+                    setMovieList([...movieList]);
+                    setMovieNotFound(false);
+                } else {
+                    setMovieNotFound(true);
+                    setMovieList([]);
+                };
+            })
+            .catch(error => console.log('error in movie data', error));
+    }
+    return {searchTitle, movieList, movieNotFound, nominations, handleSearch, handleKeyPress,
+             handleAddNominate, handleRemoveNominate}
+}
+export default useSearchForm;
